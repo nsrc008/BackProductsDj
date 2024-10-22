7d747b96bd098ce6f42b6ed9119f34d6ae4753b3
nsrc008 <nsrc008@gmail.com>
2024-10-21 21:36:51 -0500

    settings

--- ProyectApi/settings.py
+++ ProyectApi/settings.py
@@ -32,7 +32,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://backproductsdjango.onrender.com/",
+    "https://backproductsdjango.onrender.com",
 ]
 
 ALLOWED_HOSTS = ['backproductsdjango.onrender.com']
